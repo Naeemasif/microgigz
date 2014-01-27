73e03d4f7c20dc764ed5f15fd498cd0264e0104d
abdul Rehman <abdulrehman_pucit@yahoo.com>
2014-01-27 10:16:21 +0500

    NAVBAR SIDE BAR CHANGES TO APPLICATION.HTML.ERB

--- app/assets/javascripts/Application/application_functions.js
+++ app/assets/javascripts/Application/application_functions.js
@@ -13,16 +13,22 @@ function change_nav_tabs_background(x)
   if (x == "leads")
   {
       $("li>a.listerner").css("background","transparent");
-      $("#leads").css("background","black");
+      $("#leads").css("background","white");
+      $("#leads").css("border","1px solid #A1A1A1");
+      $("#leads").css("border-bottom","1px solid transparent");
   }
   else if (x == "projects")
   {
       $("li>a.listerner").css("background","transparent");
-      $("#projects").css("background","black");
+      $("#projects").css("background","white");
+      $("#projects").css("border","1px solid #A1A1A1");
+      $("#projects").css("border-bottom","1px solid transparent");
   }
   else if(x == "clients")
   {
       $("li>a.listerner").css("background","transparent");
-      $("#clients").css("background","black");
+      $("#clients").css("background","white");
+      $("#clients").css("border","1px solid #A1A1A1");
+      $("#clients").css("border-bottom","1px solid transparent");
   }
 }
