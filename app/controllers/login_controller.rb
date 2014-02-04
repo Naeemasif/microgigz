47e264efa1e1502a316a4b68660fd901831fd6c8
naeem asif <naeemgoraya@yahoo.com>
2014-02-04 18:07:05 +0500

    ldap authentication

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,47 +1,43 @@
 require 'ldap_connect'
 
 class LoginController < ApplicationController
-  skip_before_filter :authenticate
+  #skip_before_filter :authenticate
   def index
-
   end
-    def new
+  def new
+  end
 
-    end
+
   def auth
-
     @ldap = LDAP.authenticate(params[:login][:name], params[:login][:password])
     if @ldap
         session[:username] = params[:login][:name]
         @username = session[:username]
+      #sign_in(@ldap)
         flash[:notice] = "Login is scuessful!"
-         @u =  User.find_all_by_login_id(session[:username]).first
-
-      if @u.nil?
+        @existing_user =  User.find_all_by_login_id(session[:username]).first
+      if @existing_user.nil?
          redirect_to new_resource_path
         else
-              session[:current_resource_id] = @u.userable_id
-              if session[:return_to]
-                redirect_to_path(session[:return_to])
-                session[:return_to] = nil
-              else
-                redirect_to :controller => "leads"
-              end
+          session[:current_resource_id] = @existing_user.userable_id
+            if session[:return_to]
+              redirect_to_path(session[:return_to])
+              session[:return_to] = nil
+            else
+              redirect_to :controller => "leads"
+            end
       end
     else
       @user = User.find_all_by_login_id(params[:login][:name]).first
       if @user
-        sign_in(@user)
+          sign_in(@user)
       else
-      flash[:notice] = "Login failed!"
-      redirect_to :action => "new"
+          flash[:notice] = "Login failed!"
+          redirect_to :action => "new"
       end
     end
-
   end
 
-  # logout
-  # clears the session
   def signout
     session[:username] = nil
     flash[:notice] = "Logged out"
