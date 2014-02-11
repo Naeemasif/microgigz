18ff423bd2ce565f8233dd8462e41746afdf9486
abdul Rehman <abdulrehman_pucit@yahoo.com>
2014-02-11 11:36:25 +0500

    COMMIT AFTER ADDING NEW FILES

---
+++ test/functional/roleassignments_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RoleassignmentsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/role_assignment_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class RoleAssignmentHelperTest < ActionView::TestCase
+end
